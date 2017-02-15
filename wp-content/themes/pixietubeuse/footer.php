25eb053492e187aa001eff459558b01b92f58902
sandrine <sandrine.martinez1@gmail.com>
2017-02-15 02:18:02 +0100

    Ajout plugin pods pour développement avancées footer et réseaux sociaux

--- wp-content/themes/pixietubeuse/footer.php
+++ wp-content/themes/pixietubeuse/footer.php
@@ -1,7 +1,15 @@
     <footer>
         <div class="content">
-            footer
+            <?php
+            // get footer picture logo (signature picture)
+            $signaturePod = pods('signature');
+            if($signaturePod->exists()){
+                $urlSignaturePicture = $signaturePod->display( 'image' );
+            }
+            ?>
+            <img class="signature-footer" src="<?php print($urlSignaturePicture); ?>" alt="Pixietubeuse">
             <?php wp_nav_menu( array( 'theme_location' => 'reseauxSociaux' ) ); ?>
+            <div class="copyright">Copyright 2017<br /> Développé par : Jérémy Young & Sandrine Martinez</div>
         </div>
     </footer>
 <?php wp_footer(); ?>
--- wp-content/themes/pixietubeuse/modules/navigation.php
+++ wp-content/themes/pixietubeuse/modules/navigation.php
@@ -1,5 +1,6 @@
 <nav>
     <div class="content">
         <?php wp_nav_menu( array( 'theme_location' => 'menu' ) ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'reseauxSociaux' ) ); ?>
     </div>
 </nav>
